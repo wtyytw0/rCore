180bdc6a51e6de3d5e3d47b28da8335790d3481a
root <root@Taoyi-ITX.localdomain>
2020-06-24 13:52:07 +0800

    update src

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,15 @@
 global_asm!(include_str!("boot/entry64.asm"));
-
+use crate::io;
+use crate::sbi;
 #[no_mangle]
 extern "C" fn rust_main() -> ! {
+    extern "C"{
+        fn _start();
+        fn bootstacktop();
+    }
+    println!("_start vaddr = 0x{:x}", _start as usize);
+    println!("bootstacktop vaddr = 0x{:x}", bootstacktop as usize);
+    println!("hello world!");
+    panic!("you want to do nothing!");
     loop {}
 }
