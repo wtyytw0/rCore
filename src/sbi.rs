38978c74e388709770563567c34c970bbbddda40
root <root@Taoyi-ITX.localdomain>
2020-06-24 11:44:23 +0800

    add cargo, src and vscode config

---
+++ src/sbi.rs
@@ -0,0 +1,16 @@
+//! Port from sbi.h
+#![allow(dead_code)]
+
+#[inline(always)]
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let ret;
+    unsafe {
+        asm!("ecall"
+            : "={x10}" (ret)
+            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+            : "memory"
+            : "volatile");
+    }
+    ret
+}
+
